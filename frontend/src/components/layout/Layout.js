dfc54ce97093e78912a76073887642759793d985
Mico Ang <mico@apollo.com.ph>
2019-08-04 03:11:32 +0800

    [UPDATE] Updated layout and css file

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -1,17 +1,16 @@
 import React, { Fragment } from "react";
+import Logo from "../../img/clock.png";
 
 const Header = () => {
   return (
     <div className="header-container">
-      {/* <img className="header-logo" alt="Time Tracker" /> */}
-      <span className="is-title"> Time Tracker </span>
+      <div className="logo-container">
+        {/* <img src={Logo} className="header-logo" alt="Time Tracker" /> */}
+        <span className="is-title"> Time Tracker </span>
+      </div>
 
       <div className="vertical-divider" />
-      <div className="header-navigation">
-        <span> Dashboard </span>
-        <span> Employee </span>
-        <span> Projects </span>
-      </div>
+      <div className="header-navigation" />
     </div>
   );
 };
