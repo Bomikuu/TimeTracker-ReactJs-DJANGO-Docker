743f87467840c27fe845d4b1a1f6c3e48a2dd918
Mico Ang <mico@apollo.com.ph>
2019-08-03 18:03:33 +0800

    [FEAT] Added Project List Summary component

---
+++ frontend/src/components/dashboard/Recent.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Recent extends Component {
+  render() {
+    return (
+      <div className="recent-container">
+        <span> Recent Timestamps</span>
+        <div>
+          <div>Mico A. Dahang, 8 Hours</div>
+          <div>Mico A. Dahang, 8 Hours</div>
+          <div>Mico A. Dahang, 8 Hours</div>
+          <div>Mico A. Dahang, 8 Hours</div>
+          <div>Mico A. Dahang, 8 Hours</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Recent;
